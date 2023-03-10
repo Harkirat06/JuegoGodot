99bc6287fa75c4eb7c239139c97577aa7025a4d6
Peeybol <Peeybol@users.noreply.github.com>
2023-03-10 12:13:37 +0100

    cambios
    afdfdf

--- Node2D.cs
+++ Node2D.cs
@@ -6,6 +6,9 @@ public class Node2D : Godot.Node2D
 	// Declare member variables here. Examples:
 	// private int a = 2;
 	// private string b = "text";
+	
+	int a = 50;
+	double b = 100;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
